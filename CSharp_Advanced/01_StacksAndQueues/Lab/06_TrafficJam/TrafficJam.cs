8c8e0b7ce5891c4b20d3caccf6d798bba78c304c
antoniyaat <antoniyaata@gmail.com>
2018-01-25 15:14:05 +0200

    Stacks and Queues - Lab

---
+++ CSharp_Advanced/01_StacksAndQueues/Lab/06_TrafficJam/TrafficJam.cs
@@ -0,0 +1,47 @@
+﻿namespace _06_TrafficJam
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class TrafficJam
+    {
+        public static void Main()
+        {
+            var numberOfCars = int.Parse(Console.ReadLine());
+            var inputLine = Console.ReadLine();
+            var vechicles = new Queue<string>();
+            var sumCountOfCars = 0;
+
+            while (inputLine != "end")
+            {
+                if (inputLine != "green")
+                    vechicles.Enqueue(inputLine);
+
+                if (inputLine == "green")
+                {
+                    if (numberOfCars > vechicles.Count)
+                    {
+                        for (var i = 0; i < vechicles.Count; i++)
+                        {
+                            Console.WriteLine($"{vechicles.Dequeue()} passed!");
+                            sumCountOfCars++;
+                            i--;
+                        }
+                    }
+                    else
+                    {
+                        for (var i = 0; i < numberOfCars; i++)
+                        {
+                            Console.WriteLine($"{vechicles.Dequeue()} passed!");
+                            sumCountOfCars++;
+                        }
+                    }
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{sumCountOfCars} cars passed the crossroads.");
+        }
+    }
+}
